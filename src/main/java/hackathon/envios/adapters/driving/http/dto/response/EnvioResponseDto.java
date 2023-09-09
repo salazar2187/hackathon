f777395b348a081ae9ff5268544c15390c0a4461
Christian Salazar <christian.salazar@pragma.com.co>
2023-09-09 16:01:15 -0500

    bug fix

--- src/main/java/hackathon/envios/adapters/driving/http/dto/response/EnvioResponseDto.java
+++ src/main/java/hackathon/envios/adapters/driving/http/dto/response/EnvioResponseDto.java
@@ -4,12 +4,15 @@ import hackathon.envios.domain.model.Direccion;
 import hackathon.envios.domain.model.Fragilidad;
 import hackathon.envios.domain.model.Paquete;
 import hackathon.envios.domain.model.User;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@
+@NoArgsConstructor
+@AllArgsConstructor
 public class EnvioResponseDto {
     private Long id;
     private Double valorTotal;
--- src/main/java/hackathon/envios/adapters/driving/http/handlers/impl/EnvioHandlerImpl.java
+++ src/main/java/hackathon/envios/adapters/driving/http/handlers/impl/EnvioHandlerImpl.java
@@ -2,7 +2,7 @@ package hackathon.envios.adapters.driving.http.handlers.impl;
 import hackathon.envios.adapters.driving.http.dto.request.EnvioRequestDto;
 import hackathon.envios.adapters.driving.http.dto.response.EnvioResponseDto;
 import hackathon.envios.adapters.driving.http.handlers.IEnvioHandler;
-import hackathon.envios.adapters.driving.http.mappers.IEnvioRequestMapper;
+import hackathon.envios.adapters.driving.http.mappers.IEnvioResponseMapper;
 import hackathon.envios.domain.api.IEnvioServicePort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,11 +12,13 @@ import org.springframework.stereotype.Service;
 public class EnvioHandlerImpl implements IEnvioHandler {
 
     private final IEnvioServicePort envioServicePort;
-    private final IEnvioRequestMapper envioResponseMapper;
+    private final IEnvioResponseMapper envioResponseMapper;
 
 
     @Override
     public EnvioResponseDto generarEnvio(EnvioRequestDto envioRequestDto) {
-        return null;
+        return envioResponseMapper.toEnvioResponseDto(
+                envioServicePort.generarEnvio(envioRequestDto)
+        );
     }
 }
